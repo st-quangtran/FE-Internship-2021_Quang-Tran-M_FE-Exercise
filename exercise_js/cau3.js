242811c045ea08194c5c2f606aa82a84cc035d0c
st-quangtran <asdquang111@gmail.com>
2021-04-26 16:41:33 +0700

    find * to number divisible by 3

---
+++ exercise_js/cau3.js
@@ -0,0 +1,15 @@
+function cau3(input) {
+	var index = input.indexOf("*");
+	console.log(index);
+	var str1 = input.substring(0, index);
+	var str2 = input.substring(index + 1, input.length);
+	var result = [];
+	for (var i = 0; i <= 9; i++) {
+		var output = parseInt(str1 + i + str2);
+		if (output % 3 === 0) {
+			result.push(output);
+		}
+	}
+	return result;
+}
+console.log(cau3("123456789*"));
