c9c7db3bd61ea15091cb75295992e760994d959e
st-quangtran <asdquang111@gmail.com>
2021-04-26 17:13:38 +0700

    fix convention, spaces

--- exercise_js/cau1.js
+++ exercise_js/cau1.js
@@ -1,10 +1,10 @@
 function cau1(a, b) {
-	if (a === b) {
-		return a * 2 * 3;
-	}
-	else {
-		return a + b;
-	}
+  if (a === b) {
+    return a * 2 * 3;
+  }
+  else {
+    return a + b;
+  }
 }
 var a = 5;
 var b = 5;
--- exercise_js/cau2.js
+++ exercise_js/cau2.js
@@ -1,9 +1,9 @@
 function cau2(a) {
-	if (a <= 19) {
-		return 19-a;
-	}
-	else {
-		return (a-19)*3;
-	}
+  if (a <= 19) {
+    return 19 - a;
+  }
+  else {
+    return (a - 19) * 3;
+  }
 }
 console.log(cau2(20));
