862579fc9fcaaf188a553803d1e100f5657f4a33
st-quangtran <asdquang111@gmail.com>
2021-05-04 17:12:10 +0700

    Edit ES6 to ES5

--- exercise_e_shop/assets/javascript/showProduct.js
+++ exercise_e_shop/assets/javascript/showProduct.js
@@ -29,103 +29,27 @@ var listItems = [
   },
 ]
 
-var listProduct = document.getElementById("list");
+var listProduct = document.getElementById("list-products");
 var viewCountCart = document.getElementsByClassName("count-cart");
 
 if (listProduct) {
-  listItems.forEach((item) => {
-    const li = document.createElement("li");
-    const classLi = document.createAttribute("class");
-    classLi.value = "col-3 col-sm-6 list-item product-item";
-    li.setAttributeNode(classLi);
-
-    const product = document.createElement("div");
-    const classProduct = document.createAttribute("class");
-    classProduct.value = "product";
-    product.setAttributeNode(classProduct);
-
-    const linkImg = document.createElement("a");
-    const hrefImg = document.createAttribute("href");
-    hrefImg.value = "#";
-    linkImg.setAttributeNode(hrefImg);
-    linkImg.classList.add("img-product");
-
-    const imgProduct = document.createElement("img");
-    const srcImg = document.createAttribute("src");
-    srcImg.value = item.avatar;
-    const classImg = document.createAttribute("class");
-    classImg.value = "img";
-    const altImg = document.createAttribute("alt");
-    altImg.value = item.name;
-    imgProduct.setAttributeNode(srcImg);
-    imgProduct.setAttributeNode(classImg);
-    imgProduct.setAttributeNode(altImg);
-
-    const nameProduct = document.createElement("h4");
-    const className = document.createAttribute("class");
-    className.value = "product-name";
-    nameProduct.setAttributeNode(className);
-    nameProduct.innerHTML = item["name"];
-
-    const cartProduct = document.createElement("div");
-    const discountProduct = document.createElement("span");
-    const priceProduct = document.createElement("span");
-    const badgeProduct = document.createElement("span");
-    if (item["discount"] > 0) {
-      const classCart = document.createAttribute("class");
-      classCart.value = "cart-product-discount";
-      cartProduct.setAttributeNode(classCart);
-
-      var discountPrice = (item.price - item.price * 70 / 100).toFixed(2);
-      const classDiscount = document.createAttribute("class");
-      classDiscount.value = "discount-price";
-      discountProduct.setAttributeNode(classDiscount);
-      discountProduct.innerHTML = "$" + discountPrice;
-
-      const classPrice = document.createAttribute("class");
-      classPrice.value = "product-price";
-      priceProduct.setAttributeNode(classPrice);
-      priceProduct.innerHTML = "$" + item.price;
-
-      cartProduct.appendChild(discountProduct);
-      cartProduct.appendChild(priceProduct);
-
-      const classBadge = document.createAttribute("class");
-      classBadge.value = "badge";
-      badgeProduct.setAttributeNode(classBadge);
-      badgeProduct.innerHTML = item["discount"] + "%";
-    }
-    else {
-      const classPrice = document.createAttribute("class");
-      classPrice.value = "product-price";
-      priceProduct.setAttributeNode(classPrice);
-      priceProduct.innerHTML = "$" + item.price;
-    }
-
-    const btnAdd = document.createElement("button");
-    const idBtn = document.createAttribute("onclick");
-    idBtn.value = "addToCart(" + item.id + ")";
-    btnAdd.classList.add("btn");
-    btnAdd.classList.add("btn-orange");
-    btnAdd.setAttributeNode(idBtn);
-    btnAdd.innerHTML = "Add to cart";
-
-    linkImg.appendChild(imgProduct);
-
-    product.appendChild(linkImg);
-    product.appendChild(nameProduct);
-    if (item["discount"]) {
-      product.appendChild(cartProduct);
-    }
-    else {
-      product.appendChild(priceProduct);
-    }
-    if (item["discount"]) {
-      product.appendChild(badgeProduct);
-    }
-    product.appendChild(btnAdd);
-    li.appendChild(product);
-    listProduct.appendChild(li);
+  listItems.forEach(function (item) {
+    var discountPrice = (item.price - item.price * item.discount / 100).toFixed(2);
+    var viewPrice = item.discount > 0 ? '<div class="cart-product-discount">' +
+        '<p class="discount-price">$' + discountPrice + '</p>' +
+        '<p class="product-price">$' + item.price + '</p>' +
+      '</div>' +
+      '<p class="badge">-' + item.discount + '%</p>' : '<p class="product-price price">' + item.price + '</p>';
+    listProduct.innerHTML = listProduct.innerHTML + '<li class="col-3 col-sm-6 list-item product-item">' +
+      '<div class="product">' +
+        '<a href="#" class="img-product">' +
+          '<img src="' + item.avatar + '" class="img" alt="' + item.name + '">' +
+        '</a>' +
+        '<h4 class="product-name">' + item.name + '</h4>' +
+        viewPrice +
+        '<button class="btn btn-orange" onclick="addToCart(' + item.id + ')">Add to cart</button>' +
+        '</div>' +
+      '</li>';
   });
   setCountCart();
 }
@@ -135,7 +59,7 @@ function setCountCart() {
   if (listCarts) {
     if (listCarts.length > 0) {
       var countCart = 0;
-      listCarts.forEach((item) => {
+      listCarts.forEach(function (item) {
         countCart += item.number;
       })
       viewCountCart[0].innerHTML = countCart;
@@ -152,22 +76,36 @@ function setCountCart() {
 
 function addToCart(id) {
   var cart = localStorage.getItem('cart');
-  var i = listItems.findIndex(item => item.id === id);
+  var indexOfListItems;
+  for (var i = 0; i < listItems.length; i++) {
+    if (listItems[i].id === id) {
+      indexOfListItems = i;
+      break;
+    }
+  }
   var listCarts;
   if (cart) {
     listCarts = JSON.parse(cart);
-    var index = listCarts.findIndex(item => item.id === id);
-    if (index === -1) {
-      listCarts.push(listItems[i]);
+    var indexOfListCarts;
+    for (var i = 0; i < listCarts.length; i++) {
+      if (listCarts[i].id === id) {
+        indexOfListCarts = i;
+        break;
+      }
+    }
+    if (indexOfListCarts === undefined) {
+      console.log("a");
+      listCarts.push(listItems[indexOfListItems]);
       listCarts[listCarts.length - 1].number = 1;
     }
     else {
-      listCarts[index].number++;
+      console.log("b");
+      listCarts[indexOfListCarts].number++;
     }
   }
   else {
     listCarts = [];
-    listCarts.push(listItems[i]);
+    listCarts.push(listItems[indexOfListItems]);
     listCarts[listCarts.length - 1].number = 1;
   }
   localStorage.setItem('cart', JSON.stringify(listCarts));
