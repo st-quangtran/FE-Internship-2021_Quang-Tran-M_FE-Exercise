435a01b3a0fa8ab97e7f464bf8df0680a78965fd
st-quangtran <asdquang111@gmail.com>
2021-06-01 16:24:00 +0700

    Use redux to call api and get data

--- exercise_reactjs/App.tsx
+++ exercise_reactjs/App.tsx
@@ -1,24 +1,35 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import './src/assets/stylesheet/style.scss';
+import rootReducer from './src/store/rootReducer';
 
 const ListArticle = React.lazy(() => import('./src/pages/ListArticle'));
 const DetailArticle = React.lazy(() => import('./src/components/DetailArticle'));
 
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+)
+
 const App = () => {
   return (
-    <Router>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Switch>
-          <Route exact path='/'>
-            <ListArticle />
-          </Route>
-          <Route exact path='/articles/:id'>
-            <DetailArticle />
-          </Route>
-        </Switch>
-      </Suspense>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Switch>
+            <Route exact path='/'>
+              <ListArticle />
+            </Route>
+            <Route exact path='/articles/:id'>
+              <DetailArticle />
+            </Route>
+          </Switch>
+        </Suspense>
+      </Router>
+    </Provider>
   )
 }
 
